28663fa4b90cb25e31363ab8cef91413e71dea8d
PKpreeyaphat <puinune.pk@gmail.com>
2016-10-08 15:37:54 +0700

    finished

---
+++ main.rb
@@ -0,0 +1,86 @@
+US_states = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+
+abbr_states = [ ["AK", "Alaska"], 
+                ["AL", "Alabama"], 
+                ["AR", "Arkansas"], 
+                ["AS", "American Samoa"], 
+                ["AZ", "Arizona"], 
+                ["CA", "California"], 
+                ["CO", "Colorado"], 
+                ["CT", "Connecticut"], 
+                ["DC", "District of Columbia"], 
+                ["DE", "Delaware"], 
+                ["FL", "Florida"], 
+                ["GA", "Georgia"], 
+                ["GU", "Guam"], 
+                ["HI", "Hawaii"], 
+                ["IA", "Iowa"], 
+                ["ID", "Idaho"], 
+                ["IL", "Illinois"], 
+                ["IN", "Indiana"], 
+                ["KS", "Kansas"], 
+                ["KY", "Kentucky"], 
+                ["LA", "Louisiana"], 
+                ["MA", "Massachusetts"], 
+                ["MD", "Maryland"], 
+                ["ME", "Maine"], 
+                ["MI", "Michigan"], 
+                ["MN", "Minnesota"], 
+                ["MO", "Missouri"], 
+                ["MS", "Mississippi"], 
+                ["MT", "Montana"], 
+                ["NC", "North Carolina"], 
+                ["ND", "North Dakota"], 
+                ["NE", "Nebraska"], 
+                ["NH", "New Hampshire"], 
+                ["NJ", "New Jersey"], 
+                ["NM", "New Mexico"], 
+                ["NV", "Nevada"], 
+                ["NY", "New York"], 
+                ["OH", "Ohio"], 
+                ["OK", "Oklahoma"], 
+                ["OR", "Oregon"], 
+                ["PA", "Pennsylvania"], 
+                ["PR", "Puerto Rico"], 
+                ["RI", "Rhode Island"], 
+                ["SC", "South Carolina"], 
+                ["SD", "South Dakota"], 
+                ["TN", "Tennessee"], 
+                ["TX", "Texas"], 
+                ["UT", "Utah"], 
+                ["VA", "Virginia"], 
+                ["VI", "Virgin Islands"], 
+                ["VT", "Vermont"], 
+                ["WA", "Washington"], 
+                ["WI", "Wisconsin"], 
+                ["WV", "West Virginia"], 
+                ["WY", "Wyoming"] ]
+
+
+puts "' 1 '"
+    print "n = "
+    p = gets.to_i
+    if p >= 1
+        arr1 = Array.new(p) {|i| (i+1)**3 }
+        puts "Member of array is "+arr1.inspect
+    else 
+        puts "Input number less than 1"
+    end
+
+puts "' 4 '"
+    arr4 = Array.new(US_states)
+    arr4.each {|value| puts value if value.length > 8}
+
+puts "' 5 '"
+    arr5 = Array.new(US_states)
+    arr5.each {|key,value| puts key if (['M','N','S'].include? key[0]) && (['a','e','i','o','u'].include? key[key.length-1])}
+
+puts "' 7 '"
+    hash = Hash[*abbr_states.flatten]
+    hash.each{|key,value| puts key+" "+value}
+
+
+puts "' 8 '"
+    hash = Hash[*abbr_states.flatten]
+    hash.each{|key,value| puts key+" "+value if key.casecmp(value[0..1]) == 0}
+
